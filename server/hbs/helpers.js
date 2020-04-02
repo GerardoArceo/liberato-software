c8c0313ca474f4ae875161f38f5dc0ca4133944d
= <=>
2020-04-02 09:30:00 -0600

    Mejoras de diseño

--- server/hbs/helpers.js
+++ server/hbs/helpers.js
@@ -4,3 +4,19 @@ const hbs = require('hbs');
 hbs.registerHelper('getYear', () => {
     return new Date().getFullYear();
 });
+
+hbs.registerHelper('active', (n1, n2) => {
+    if (n1 == n2) {
+        return 'active';
+    } else {
+        return '';
+    }
+});
+
+hbs.registerHelper('s', (p1, p2) => {
+    if (p1) {
+        return p1;
+    } else {
+        return p2;
+    }
+});
--- server/server.js
+++ server/server.js
@@ -12,34 +12,75 @@ app.use(express.static(__dirname + '/../public'));
 hbs.registerPartials(__dirname + '/../views/parcials');
 app.set('view engine', 'hbs');
 
-app.get('/home', (req, res) => res.render('home'));
-app.get('/development', (req, res) => res.render('development'));
-app.get('/contact', (req, res) => res.render('contact'));
-app.get('/team', (req, res) => res.render('team'));
-app.get('/blog', (req, res) => res.render('blog'));
+app.get('/home', (req, res) => res.render('home', {
+    activeIndex: 0,
+}));
+app.get('/development', (req, res) => res.render('development', {
+    title: 'Desarrollo LS',
+    activeIndex: 0,
+}));
+app.get('/team', (req, res) => res.render('team', {
+    title: 'Equipo LS',
+    activeIndex: 1,
+}));
+app.get('/contact', (req, res) => res.render('contact', {
+    title: 'Contacto LS',
+    activeIndex: 1,
+}));
+app.get('/blog', (req, res) => res.render('blog', {
+    title: 'Blog LS',
+    activeIndex: 2,
+}));
 
-app.get('/projects/piase', (req, res) => res.render('projects/piase'));
-app.get('/projects/lienzourbano', (req, res) => res.render('projects/lienzourbano'));
-app.get('/projects/lietoapp', (req, res) => res.render('projects/lietoapp'));
-app.get('/projects/ilaw', (req, res) => res.render('projects/ilaw'));
+app.get('/projects/piase', (req, res) => res.render('projects/piase', {
+    title: 'PIASE',
+    theme: 'light',
+    activeIndex: 0,
+}));
+app.get('/projects/lienzourbano', (req, res) => res.render('projects/lienzourbano', {
+    title: 'Lienzo Urbano',
+    theme: 'light',
+    activeIndex: 0,
+}));
+app.get('/projects/lietoapp', (req, res) => res.render('projects/lietoapp', {
+    title: 'Lie to App',
+    theme: 'light',
+    activeIndex: 0,
+}));
+app.get('/projects/ilaw', (req, res) => res.render('projects/ilaw', {
+    title: 'iLaw',
+    theme: 'light',
+    activeIndex: 0,
+}));
 
-app.get('/team/gerardoarceo', (req, res) => res.render('team/gerardoarceo'));
-app.get('/team/lilianarceo', (req, res) => res.render('team/lilianarceo'));
-app.get('/team/marcocelaya', (req, res) => res.render('team/marcocelaya'));
-app.get('/team/pamelaperez', (req, res) => res.render('team/pamelaperez'));
+app.get('/gerardoarceo', (req, res) => res.render('team/gerardoarceo', {
+    title: 'Gerardo Arceo',
+    activeIndex: 1,
+}));
+app.get('/lilianarceo', (req, res) => res.render('team/lilianarceo', {
+    title: 'Lilián Arceo',
+    activeIndex: 1,
+}));
+app.get('/marcocelaya', (req, res) => res.render('team/marcocelaya', {
+    title: 'Marco Celaya',
+    activeIndex: 1,
+}));
+app.get('/pamelaperez', (req, res) => res.render('team/pamelaperez', {
+    title: 'Pamela Pérez',
+    activeIndex: 1,
+}));
 
 app.get('*', (req, res) => {
     const host = req.headers.host;
     if (host.includes('gerardoarceo'))
-        res.render('team/gerardoarceo');
-    else if (host.includes('lilianarceo'))
-        res.render('team/lilianarceo');
-    else if (host.includes('marcocelaya'))
-        res.render('team/marcocelaya');
-    else if (host.includes('pamelaperez'))
-        res.render('team/pamelaperez');
+        res.render('team/gerardoarceo', {
+            title: 'Gerardo Arceo',
+            activeIndex: 1,
+        });
     else
-        res.render('home');
+        res.render('home', {
+            activeIndex: 0,
+        });
 });
 
 app.listen(port, console.log(`SERVER LISTENING PORT: ${ port }`));
