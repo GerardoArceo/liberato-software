98c356bc82f4ca8ddb91800f539bd65729d6bbc1
= <=>
2020-06-15 21:07:39 -0500

    commit

--- server/server.js
+++ server/server.js
@@ -6,10 +6,10 @@ require('./hbs/helpers');
 
 const port = 3000;
 
-app.use(express.static(__dirname + '/../public'));
+app.use(express.static(__dirname + '/public'));
 
 // Express HBS engine
-hbs.registerPartials(__dirname + '/../views/parcials');
+hbs.registerPartials(__dirname + '/views/parcials');
 app.set('view engine', 'hbs');
 
 app.get('/home', (req, res) => res.render('pages/home', {
